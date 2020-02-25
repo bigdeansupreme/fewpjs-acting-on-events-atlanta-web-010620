532c4400299e57a1ea4f1fdbcad305e2a310f541
Dino <dinokunovac@gmail.com>
2020-02-25 12:35:08 -0500

    Done.

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ function moveDodgerLeft() {
     }
 }
 
-// document.addEventListener('keydown', function(e) {
-//     if (e.key === 'ArrowLeft') {
-//         moveDodgerLeft();
-//     }
-// });
-
 function moveDodgerRight() {
     let leftNumbers = dodger.style.left.replace("px", "")
     let left = parseInt(leftNumbers, 10)
